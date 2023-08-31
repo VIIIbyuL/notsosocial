9e293eaed89937905469b1120059f42022b2dbf8
VIIIbyuL <myjjk03@gmail.com>
2023-08-30 23:15:37 -0400

    more styling changes

--- src/pages/DisplaySearch.tsx
+++ src/pages/DisplaySearch.tsx
@@ -11,17 +11,19 @@ export default function DisplaySearch({
 }) {
   return (
     <div>
-      {searchData.map((item, index) => (
-        <div
-          key={index}
-          className="mb-5 w-[350px] max-w-md rounded-lg bg-slate-600 p-5 text-white shadow-md"
-        >
-          <h3 className="text-xl font-semibold">Username: {item.name}</h3>
-          <p className="text-gray-200">Email: {item.email}</p>
-
-          {/* <img src={item.image} alt={`Image for ${item.name}`} className="rounded-full mt-3" /> */}
-        </div>
-      ))}
+      {searchData.length > 0 ? (
+        searchData.map((item, index) => (
+          <div
+            key={index}
+            className="mb-5 w-[350px] max-w-md rounded-lg bg-slate-600 p-5 text-white shadow-md"
+          >
+            <h3 className="text-xl font-semibold">Username: {item.name}</h3>
+            <p className="text-gray-200">Email: {item.email}</p>
+          </div>
+        ))
+      ) : (
+        <p>No users found</p>
+      )}
     </div>
   );
 }
